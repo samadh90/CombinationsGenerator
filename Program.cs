838fd7dd9626a5ab0939a521888fb441bb880ab5
Samad Hatsijev <s.hatsijev@hotmail.com>
2023-01-25 20:00:05 +0100

    Pour combler le trou

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace CombinationsGenerator;
 
 public class Program
 {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
